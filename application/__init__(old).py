06c404bb211bb5ba9ceb46449710983d394fd0bf
sharma-sid01 <sharma.sid01@gmail.com>
2025-06-22 17:46:23 +0300

    Modified model

--- application/__init__(old).py
+++ application/__init__(old).py
@@ -33,6 +33,10 @@ weight_std_scale = StandardScaler()
 aphi_std_scale = StandardScaler()
 aplo_std_scale = StandardScaler()
 cholesterol_std_scale = StandardScaler()
+gluc_std_scale = StandardScaler()
+smoke_std_scale = StandardScaler()
+alco_std_scale = StandardScaler()
+active_std_scale = StandardScaler()
 
 df['age'] = age_std_scale.fit_transform(df[['age']])
 df['gender'] = gender_std_scale.fit_transform(df[['gender']])
@@ -41,6 +45,10 @@ df['weight'] = weight_std_scale.fit_transform(df[['weight']])
 df['ap_hi'] = aphi_std_scale.fit_transform(df[['ap_hi']])
 df['ap_lo'] = aplo_std_scale.fit_transform(df[['ap_lo']])
 df['cholesterol'] = cholesterol_std_scale.fit_transform(df[['cholesterol']])
+df['gluc'] = gluc_std_scale.fit_transform(df[['gluc']])
+df['smoke'] = smoke_std_scale.fit_transform(df[['smoke']])
+df['alco'] = alco_std_scale.fit_transform(df[['alco']])
+df['active'] = active_std_scale.fit_transform(df[['active']])
 
 # Modeling - Logistic Regression
 X = df[['age','gender','height','weight','ap_hi','ap_lo','cholesterol','gluc','smoke','alco','active']]
@@ -53,6 +61,8 @@ model.fit(X_train, y_train)
 #Create flask instance
 app = Flask(__name__)
 
+#if __name__ == "__main__":
+#    app.run(debug=True)
 
 #Create API
 @app.route('/api', methods=['GET','POST'])
@@ -89,13 +99,25 @@ def predict():
     data_cholesterol = np.array(cholesterol_std_scale.transform(data_cholesterol))
 
     data_gluc = np.array([data["gluc"]])
+    data_gluc= np.reshape(data_gluc, (1, -1))
+    data_gluc = np.array(gluc_std_scale.transform(data_gluc))
+
     data_smoke = np.array([data["smoke"]])
+    data_smoke= np.reshape(data_smoke, (1, -1))
+    data_smoke = np.array(aphi_std_scale.transform(data_smoke))
+
     data_alco = np.array([data["alco"]])
+    data_alco= np.reshape(data_alco, (1, -1))
+    data_alco = np.array(aplo_std_scale.transform(data_alco))
+
     data_active = np.array([data["active"]])
+    data_active= np.reshape(data_active, (1, -1))
+    data_active = np.array(active_std_scale.transform(data_active))
 
     data_final = np.column_stack((data_age, data_gender, data_height, data_weight, data_ap_hi, data_ap_lo, data_cholesterol, data_gluc, data_smoke, data_alco, data_active))
     data_final = pd.DataFrame(data_final, dtype=object)
 
-    #make prediction using model
+    #make predicon using model
     prediction = model.predict(data_final)
+    # return Response(json.dumps(prediction[0]))
     return Response(json.dumps(int(prediction[0])))
